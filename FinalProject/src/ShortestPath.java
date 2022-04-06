554255da93ed65087cf6953701119b901ed04a54
Ciara <Ciara@192.168.0.38>
2022-04-06 15:38:08 +0100

    Updated shortestPath

--- FinalProject/src/ShortestPath.java
+++ FinalProject/src/ShortestPath.java
@@ -1,19 +1,28 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class ShortestPath 
 
 {
 
-	private double matrix[][];
-	private double distTo[][];
-	private int[][] edgeTo[][];
+	private static double matrix[][];
+	private static double distTo[];
+	private static int[] edgeTo;
+	static int visited[];
 	private int stopCost = 1;
 	private int immediateTransCost = 2;
-	public double inf = Double.POSITIVE_INFINITY;
+	public static double inf = Double.POSITIVE_INFINITY;
 
 	//cannot yet initialize type 2 cost as need minTime
+	
+	
+	public static void main(String[] arg) {
+		
+		shortestPath(646,379);
+		
+	}
 
 	
 	public void createMatrix() throws FileNotFoundException {
@@ -90,4 +99,88 @@ public class ShortestPath
 	}
 		scanTransfers.close();
 }
+	
+	public static String shortestPath(int depart, int arrive){
+		
+		String output = " ";
+		visited[depart] = 1;
+		distTo[depart] = 0;
+		int current = depart;
+		int stopsVisited = 0;
+		double minDist = inf;
+		
+		if (depart == arrive) {
+			
+			output = "Departure Stop: " + depart
+					+ "\nArrival Stop: " + arrive
+					+ "Minimum cost: " + matrix[depart][arrive]
+					+ "No other stops on this route.";
+			return output;
+		}
+		
+		else {
+			int n  = distTo.length;
+			for(int i = 0; i < n; i++) {
+	    		if(i != depart)
+	    		{
+	    			distTo[i] = inf;
+	    		}
+	    	}
+			
+			while (stopsVisited < n) {
+				
+				for(int i = 0; i < n; i++) 
+				{
+					if(visited[i] != 1 && distTo[i] < minDist) 
+					{
+						current = i;
+						minDist = distTo[i];
+					}
+				}
+				stopsVisited++;
+				
+				for(int i = 0; i < matrix[current].length; i ++) 
+				{
+
+					if(visited[i] == 0) 
+					{
+						relaxEdge(current, i, distTo, edgeTo);
+					}
+
+				}
+				visited[current] = 1;
+			}
+			
+			if(distTo[arrive] ==inf) 
+			{
+				return "No route possible.";
+			}
+			
+			int x = depart;
+			int y = arrive;
+			String via = "";
+			
+			while(x != y) 
+			{
+				via =  edgeTo[y] + via;
+				y = edgeTo[y];
+			}
+			
+			output = "Departure Stop: " + depart
+					+ "\nArrival Stop: " + arrive
+					+ "Minimum cost: " + matrix[depart][arrive]
+					+ "Route via: " + via;
+
+			return output;
+		}
+
+	}
+	
+	 private static void relaxEdge(int depart, int arrive, double[] distTo, int[] edgeTo) {
+	    	if(distTo[arrive] > matrix[depart][arrive]+ distTo[depart] ) {
+	    		distTo[depart] = matrix[depart][arrive] + distTo[depart] ;
+	    		edgeTo[arrive] = depart;
+	    	}
+	    }
+
 }
